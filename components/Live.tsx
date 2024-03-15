6a9d333388a4c68bd8d5dcb837b0c7f62318f975
primedev88 <anujk7.dev@gmail.com>
2024-03-15 22:53:30 +0530

    first commit

---
+++ components/Live.tsx
@@ -0,0 +1,60 @@
+// collections of all live functionalities
+import { useMyPresence, useOthers } from "@/liveblocks.config"
+import LiveCursors from "./cursor/LiveCursors"
+import { useCallback, useState } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
+
+const Live = () => {
+  const others = useOthers(); //returns all the users currently in the same room
+  const [{ cursor },updateMyPresence] = useMyPresence() as any;
+  
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  })
+  const handlePointerMove = useCallback((event:React.PointerEvent)=>{
+    event.preventDefault();
+
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({cursor:{x,y} });
+  },[])
+
+  const handlePointerLeave = useCallback((event:React.PointerEvent)=>{
+    setCursorState({ mode: CursorMode.Hidden })
+
+    updateMyPresence({cursor: null, message: null });
+  },[])
+
+  const handlePointerDown = useCallback((event:React.PointerEvent)=>{
+    
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({cursor:{x,y} });
+  },[])
+
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center "
+    >
+      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1> 
+      <LiveCursors others={others} />
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
+    </div>
+  )
+}
+
+export default Live
