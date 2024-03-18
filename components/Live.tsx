c3e093d41a3a09c98430dc8f0d73b196f99ea1b5
primedev88 <anujk7.dev@gmail.com>
2024-03-18 19:33:13 +0530

    worked

--- components/Live.tsx
+++ components/Live.tsx
@@ -8,7 +8,11 @@ import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+}
+
+const Live = ({ canvasRef }: Props) => {
   const others = useOthers(); //returns all the users currently in the same room
   const [{ cursor },updateMyPresence] = useMyPresence() as any;
   
@@ -124,13 +128,14 @@ const Live = () => {
 
   return (
     <div
+      id="canvas"
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
       className="h-[100vh] w-full flex justify-center items-center text-center "
     >
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1> 
+      <canvas ref={canvasRef}/>
       {reaction.map((r)=>(
         <FlyingReaction
           key={r.timestamp.toString()}
--- components/users/Avatar.tsx
+++ components/users/Avatar.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 export function Avatar({  name, otherStyles }: { otherStyles: string; name: string }) {
   return (
-    <div className={styles.avatar} data-tooltip={name}>
+    <div className={`${styles.avatar} ${otherStyles} h-9 w-9`} data-tooltip={name}>
       <Image
         src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
-        
+        fill
         className={styles.avatar_picture}
         alt={name}
       />
